84c75cb3272e695904c5e89705d52c7f649b7446
alilo113 <aliopdandan@gmail.com>
2025-10-23 11:34:51 +0100

    f

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 root = tk.Tk()  # Create main window
 frm = ttk.Frame(root, padding=100)
-ttk.Label(frm, text="Hello world").grid(column=0, row=0)
+ttk.Button(frm, text="Genarate Password").pack()
 frm.pack()      # Make the frame visible
 
 root.mainloop()  # Start the GUI event loop
