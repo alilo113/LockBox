d3ce2951d0ca70b0364636693bea39b258f28d8b
alilo113 <aliopdandan@gmail.com>
2025-10-24 12:31:24 +0100

    f

--- main/generatePasswd.py
+++ main/generatePasswd.py
@@ -1,2 +1,13 @@
-passwordChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', '"', "'", '<', '>', ',', '.', '?', '/']
+import random
+
+passwordChars = ['a',"b","c" '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', '"', "'", '<', '>', ',', '.', '?', '/']
 length = 16
+
+def generatePassword():
+    password = "";
+    for i in range(length):
+        password += random.choice(passwordChars)
+    
+    return password
+
+print(f"Genarated Password: {generatePassword()}")
