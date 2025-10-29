526f4792b093a30e525d75342989d1907c952582
alilo113 <aliopdandan@gmail.com>
2025-10-29 09:15:53 +0100

    f

--- client/src/components/signup.tsx
+++ client/src/components/signup.tsx
@@ -2,6 +2,20 @@ import React from "react"
 
 export function Signup(){
     return(
-        <h1 className="text-red-500">hello world</h1>
+        <div>
+            <h1>Create your account</h1>
+            <form action="">
+                <label htmlFor="email">Email: </label>
+                <input type="text" id="email" name="email" className="" />
+
+                <label htmlFor="username">username: </label>
+                <input type="text" id="username" name="username" className="" />
+
+                <label htmlFor="password">password: </label>
+                <input type="text" id="password" name="password" className="" />
+
+                <button type="submit">Create Account</button>
+            </form>
+        </div>
     )
 }
