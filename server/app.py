cae32f0fb83772bd9365d717f1a1c7b7a1c7984a
alilo113 <aliopdandan@gmail.com>
2025-10-30 09:46:38 +0100

    f

--- server/app.py
+++ server/app.py
@@ -3,8 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, world!"
+def home():
+    return "THis is homepage"
+    
+@app.route("/signup")
+def signup():
+    return "Signup page!"
+
+@app.route("/login")
+def login():
+    return "Login page!"
 
 if __name__ == "__main__":
     app.run()
