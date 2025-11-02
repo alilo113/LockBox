bc65abf528d2064589152d3127fdf8a5ac726cb3
alilo113 <aliopdandan@gmail.com>
2025-11-02 18:35:13 +0100

    f

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Server } from 'http'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  server: {
+    proxy: {
+      "/": "http://127.0.0.1:5000"
+    }
+  }
 })
